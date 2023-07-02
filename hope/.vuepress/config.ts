246cfb2917039db58f32f803f1be52b527292341
SmallPigPig <3552354372@qq.com>
2023-07-02 17:04:41 +0800

    no message

--- hope/.vuepress/config.ts
+++ hope/.vuepress/config.ts
@@ -1,12 +1,42 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 import { searchPlugin } from '@vuepress/plugin-search'
+import { getDirname, path } from "@vuepress/utils";
 
 export default defineUserConfig({
   base: "/",  
   lang: "zh-CN",
   title: "AlphaBlog",
   description: "AlphaBlog 是为所有人提供的学习网站",
+  head: [
+    // 导入相应链接
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,300&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+
+    // 导入相应链接
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+  ],
 
   theme,
   plugins: [
@@ -24,4 +54,4 @@ export default defineUserConfig({
 
   // Enable it with pwa
   // shouldPrefetch: false,
-})
+});
