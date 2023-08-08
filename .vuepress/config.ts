5bf13775a66ff2431c427eb6192fead39ddd2048
ShiYiZhiHanZhuXhuYa <3552354372@qq.com>
2023-08-09 02:24:41 +0800

    主题更换

---
+++ .vuepress/config.ts
@@ -0,0 +1,103 @@
+import { defineUserConfig } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+
+export default defineUserConfig({
+  title: "Alpha Blog",
+  description: "为所有人服务的学习网站",
+  theme: recoTheme({
+    autoSetSeries: true,
+    colormode: "dark",
+    style: "@vuepress-reco/style-default",
+    logo: "/logo.png",
+    author: "SmallPigPig",
+    authorAvatar: "/head.png",
+    docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+    docsBranch: "main",
+    docsDir: "example",
+    lastUpdatedText: "",
+    // series 为原 sidebar
+    series: {
+      '/docs/document/': [
+          {
+              text: '八年级',
+              children: ['/docs/document/八年级/8A.md', '/docs/document/八年级/8B.md']
+          },
+          {
+              text: "九年级",
+              children: ['/docs/document/九年级/9A.md', '/docs/document/九年级/9B.md']
+          }
+      ],
+
+      '/docs/about/': [
+        {
+          text: "关于文档",
+          children: ['/docs/about/关于文档/关于网站.md', '/docs/about/关于文档/关于作者.md']
+        },
+        {
+          text: "文档优势",
+          children: ['/docs/about/文档优势/标准的语法.md', '/docs/about/文档优势/简洁的代码.md', '/docs/about/文档优势/清晰的目标.md', '/docs/about/文档优势/整齐的分类.md']
+        }
+      ]
+    },
+    navbar: [
+      {
+        text: '文档',
+        icon: 'Document',
+        children: [
+          {
+            text: '八年级',
+            children: [
+              { text: '八年级上册', link: '/docs/document/八年级/8A.md' },
+              { text: '八年级下册', link: '/docs/document/八年级/8B.md' },
+            ],
+          },
+          {
+            text: '九年级',
+            children: [
+              { text: '九年级上册', link: '/docs/document/九年级/9A.md' },
+              { text: '九年级下册', link: '/docs/document/九年级/9B.md' },
+            ],
+          },
+        ],
+      },
+
+      {
+        text: '关于',
+        icon: 'Account',
+        children: [
+          {
+            text: '关于文档',
+            children: [
+              { text: '关于网站', link: '/docs/about/关于文档/关于网站.md' },
+              { text: '关于作者', link: '/docs/about/关于文档/关于作者.md' },
+            ],
+          },
+          {
+            text: "文档优势",
+            children: [
+              { text: '标准的语法', link: '/docs/about/文档优势/标准的语法.md' },
+              { text: '简洁的代码', link: '/docs/about/文档优势/简洁的代码.md' },
+              { text: '清晰的目标', link: '/docs/about/文档优势/清晰的目标.md' },
+              { text: '整齐的分类', link: '/docs/about/文档优势/整齐的分类.md' },
+            ]
+          }
+        ]
+      },
+    ],
+
+    // commentConfig: {
+    //   type: 'valie',
+    //   // options 与 1.x 的 valineConfig 配置一致
+    //   options: {
+    //     // appId: 'xxx',
+    //     // appKey: 'xxx',
+    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
+    //     // verify: true, // 验证码服务
+    //     // notify: true,
+    //     // recordIP: true,
+    //     // hideComments: true // 隐藏评论
+    //   },
+    // },
+  }),
+  // debug: true,
+});
---
+++ README.md
@@ -0,0 +1,46 @@
+---
+home: true
+modules:
+  - BannerBrand
+  - Footer
+bannerBrand:
+  bgImage: '/bg.svg'
+  title: AlphaBlog
+  description: 为所有人服务的学习网站
+  tagline: AlphaBlog 将继续坚持美观，简洁，明了的宗旨。为所有人提供优秀的文档阅览体验
+  buttons:
+    - { text: 阅读文档, link: '/docs/document/八年级/8B' }
+    - { text: 关于文档, link: '/docs/about/关于文档/关于网站', type: 'plain' }
+  socialLinks:
+    - { icon: 'LogoGithub', link: 'https://github.com/vuepress-reco/vuepress-theme-reco' }
+footer:
+  startYear: 2023
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+---
+
+## 快速开始
+
+**npx**
+
+```bash
+# 初始化，并选择 2.x
+npx @vuepress-reco/theme-cli init
+```
+
+**npm**
+
+```bash
+# 初始化，并选择 2.x
+npm install @vuepress-reco/theme-cli@1.0.7 -g
+theme-cli init
+```
+
+**yarn**
+
+```bash
+# 初始化，并选择 2.x
+yarn global add @vuepress-reco/theme-cli@1.0.7
+theme-cli init
+```
