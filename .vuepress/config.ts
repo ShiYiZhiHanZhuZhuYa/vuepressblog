1d7e2a8cc4afc1a6f536b8b796e7eb78557f7813
ShiYiZhiHanZhuXhuYa <3552354372@qq.com>
2023-09-16 10:51:56 +0800

    修复文档

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -3,6 +3,7 @@ import { defineUserConfig } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
 import { componentsPlugin } from "vuepress-plugin-components";
 import { sitemapPlugin } from "vuepress-plugin-sitemap2";
+import { readingTimePlugin } from "vuepress-plugin-reading-time2";
 
 export default defineUserConfig({
   title: "Alpha Blog",
@@ -26,6 +27,9 @@ export default defineUserConfig({
         // 配置选项
         hostname: "https://alphaalist.xyz"
       }),
+    readingTimePlugin({
+        // 你的选项
+      }),
   ],
   theme: recoTheme({
     style: "@vuepress-reco/style-default",
@@ -40,33 +44,33 @@ export default defineUserConfig({
     // series 为原 sidebar
 
     series: {
-      '/docs/document/': [
+      '/docs/docs/document/': [
           {
             text: '文档的说明',
-            children: ['/docs/document/beforeread.html', '/docs/document/rule.html', '/docs/document/use.html', '/docs/document/pull.html'] 
+            children: ['/docs/docs/document/beforeread.html', '/docs/docs/document/rule.html', '/docs/docs/document/use.html', '/docs/docs/document/pull.html'] 
           },
           {
             text: '七年级资料',
-            children: ['/docs/document/7/7a.html', '/docs/document/7/7b.html']
+            children: ['/docs/docs/document/7/7a.html', '/docs/docs/document/7/7b.html']
           },
           {
               text: '八年级资料',
-              children: ['/docs/document/8/8a.html', '/docs/document/8/8b.html']
+              children: ['/docs/docs/document/8/8a.html', '/docs/docs/document/8/8b.html']
           },
           {
               text: "九年级资料",
-              children: ['/docs/document/9/9a.html', '/docs/document/9/9b.html']
+              children: ['/docs/docs/document/9/9a.html', '/docs/docs/document/9/9b.html']
           }
       ],
 
-      '/docs/about/': [
+      '/docs/docs/about/': [
         {
           text: "关于文档",
-          children: ['/docs/about/关于文档/关于网站.html', '/docs/about/关于文档/关于作者.html']
+          children: ['/docs/docs/about/关于文档/关于网站.html', '/docs/docs/about/关于文档/关于作者.html']
         },
         {
           text: "文档优势",
-          children: ['/docs/about/文档优势/标准的语法.html', '/docs/about/文档优势/简洁的代码.html', '/docs/about/文档优势/清晰的目标.html', '/docs/about/文档优势/整齐的分类.html']
+          children: ['/docs/docs/about/文档优势/标准的语法.html', '/docs/docs/about/文档优势/简洁的代码.html', '/docs/docs/about/文档优势/清晰的目标.html', '/docs/docs/about/文档优势/整齐的分类.html']
         }
       ]
     },
@@ -79,22 +83,22 @@ export default defineUserConfig({
           {
             text: '七年级资料',
             children: [
-              { text: '七年级上册', link: '/docs/document/7/7a.md' },
-              { text: '七年级下册', link: '/docs/document/7/7b.md' },
+              { text: '七年级上册', link: '/docs/docs/document/7/7a.md' },
+              { text: '七年级下册', link: '/docs/docs/document/7/7b.md' },
             ],
           },
           {
             text: '八年级资料',
             children: [
-              { text: '八年级上册', link: '/docs/document/8/8a.md' },
-              { text: '八年级下册', link: '/docs/document/8/8b.md' },
+              { text: '八年级上册', link: '/docs/docs/document/8/8a.md' },
+              { text: '八年级下册', link: '/docs/docs/document/8/8b.md' },
             ],
           },
           {
             text: '九年级资料',
             children: [
-              { text: '九年级上册', link: '/docs/document/9/9a.md' },
-              { text: '九年级下册', link: '/docs/document/9/9b.md' },
+              { text: '九年级上册', link: '/docs/docs/document/9/9a.md' },
+              { text: '九年级下册', link: '/docs/docs/document/9/9b.md' },
             ],
           },
           
@@ -108,22 +112,22 @@ export default defineUserConfig({
           {
             text: '关于文档',
             children: [
-              { text: '关于网站', link: '/docs/about/关于文档/关于网站.md' },
-              { text: '关于作者', link: '/docs/about/关于文档/关于作者.md' },
+              { text: '关于网站', link: '/docs/docs/about/关于文档/关于网站.md' },
+              { text: '关于作者', link: '/docs/docs/about/关于文档/关于作者.md' },
             ],
           },
           {
             text: "文档优势",
             children: [
-              { text: '标准的语法', link: '/docs/about/文档优势/标准的语法.md' },
-              { text: '简洁的代码', link: '/docs/about/文档优势/简洁的代码.md' },
-              { text: '清晰的目标', link: '/docs/about/文档优势/清晰的目标.md' },
-              { text: '整齐的分类', link: '/docs/about/文档优势/整齐的分类.md' },
+              { text: '标准的语法', link: '/docs/docs/about/文档优势/标准的语法.md' },
+              { text: '简洁的代码', link: '/docs/docs/about/文档优势/简洁的代码.md' },
+              { text: '清晰的目标', link: '/docs/docs/about/文档优势/清晰的目标.md' },
+              { text: '整齐的分类', link: '/docs/docs/about/文档优势/整齐的分类.md' },
             ]
           }
         ]
       },
-      { text: '音乐', link: '/docs/heomusic.html', icon: 'Music' },
+      { text: '音乐', link: '/docs/docs/heomusic.html', icon: 'Music' },
     ],
 
     algolia: {
--- README.md
+++ README.md
@@ -9,8 +9,8 @@ bannerBrand:
   description: 为所有人服务的学习网站
   tagline: AlphaBlog 将继续坚持美观，简洁，明了的宗旨。为所有人提供优秀的文档阅览体验
   buttons:
-    - { text: 阅读文档, link: '/docs/document/BeforeRead.html' }
-    - { text: 关于文档, link: '/docs/about/关于文档/关于网站.html', type: 'plain' }
+    - { text: 阅读文档, link: '/docs/docs/document/BeforeRead.html' }
+    - { text: 关于文档, link: '/docs/docs/about/关于文档/关于网站.html', type: 'plain' }
   socialLinks:
     - { icon: 'LogoGithub', link: 'https://github.com/ShiYiZhiHanZhuZhuYa/vuepressblog' }
 footer:
